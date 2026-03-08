47bf80c3c703f5a5b9724022faf66492f96726bd
KateC08 <coming.katelyn08@gmail.com>
2026-03-08 20:06:12 +0800

    Added a function to fetch the data on the database

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const supabaseUrl = "https://saawgnizjfsepeooahsk.supabase.co";
 const supabaseKey = "sb_publishable_H5IRLcvWEIFWWFC1pUI2fw_lmDJD9V_";
 
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
+
+async function fetchWeather() {
+}
